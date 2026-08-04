a99351c8de34673d3af4b641e575cb5d90726e36
a4techvisual <a4techvisual.a4techvisual@gmail.com>
2026-08-03 21:32:32 -0300

    Aula do dia 03/08/2026

---
+++ Parte02-Colecoes/01_lista/main.py
@@ -0,0 +1,11 @@
+# lista de nomes
+nomes = ["Alex", "Jesus", "Lucinao", "Raquel", "Rose"]
+
+# exibe o primeiro elemento da lista
+# print(nomes[0])
+# print(nomes[4])
+
+for nome in nomes:
+    print(nome)
+
+
