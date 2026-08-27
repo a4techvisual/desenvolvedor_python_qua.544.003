5ed82a7e3d108bfe770a771b3a4e39d799e987a0
A4techvisual <a4techvisual@gmail.com>
2026-08-26 21:40:52 -0300

    Aula do dia 26/08/2026.

---
+++ Parte04-Orientacao-a-Objetos/09_composicao/main.py
@@ -0,0 +1,11 @@
+from models import Carro
+
+def main():
+    carro = Carro(modelo="", potencia="")''
+
+    carro.modelo = input("Informe o modelo do carro: ").strip().title()
+    carro.motor.potencia = input("Informe a potência do motor: ").strip()
+    print(carro.detalhes())
+
+    if __name__ == "__main__":
+        main()
