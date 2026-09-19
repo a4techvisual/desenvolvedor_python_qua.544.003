dddfe1f61f597e8a765e2202a328ee16353ba8ea
A4techvisual <a4techvisual@gmail.com>
2026-09-18 21:34:11 -0300

    Commit 18/09/2026

--- Parte07-Flask/05_captura_de_texto_ocr/app.py
+++ Parte07-Flask/05_captura_de_texto_ocr/app.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template, request
 
-
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
+@app.route("/textoExtraido")
+def extrair_texto():
+    return render_template("textExtraido.html")
 
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
